ad85456e2389ee246a7498b2001922ef65545327
Éder Luciano da Costa <miibx5@gmail.com>
2020-08-22 17:06:57 -0300

    Creating Project

---
+++ src/main/java/br/com/edersystems/schedulechallenge/ScheduleChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.edersystems.schedulechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleChallengeApplication.class, args);
+	}
+
+}
